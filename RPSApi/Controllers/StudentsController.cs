add71cd11c0d4f7a7aa6d8eee639839446096ed5
sakshamojha <sakshamojha@SAKSHAMOJHA8CD8>
2025-04-12 18:46:01 +0530

    Add project files.

---
+++ RPSApi/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RPSApi.Models;
+
+namespace RPSApi.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> students = new List<Student>();
+
+        [HttpPost("admission")]
+        public IActionResult NewAdmission([FromBody] Student student)
+        {
+            students.Add(student);
+            return Ok("Admission Successful");
+        }
+
+        [HttpGet("allAdmissions")]
+        public IActionResult GetStudents()
+        {
+            return Ok(students);
+        }
+    }
+}
---
+++ RPSApi/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace RPSApi.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+    }
+}
