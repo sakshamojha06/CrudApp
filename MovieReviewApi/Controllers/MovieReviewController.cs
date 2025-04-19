a75b3a592075b8ef771132ae6d4731bd2ae0c626
Saksham Ojha <sakshamojha2006@outlook.com>
2025-04-19 08:49:19 +0530

    Add ToDoApi and MovieReviewApi projects
    - Introduced new projects: ToDoApi and MovieReviewApi.
    - Updated solution configuration for Debug and Release setups.
    - Added logging configuration in appsettings files.
    - Created HTTP files for API endpoint testing.
    - Implemented Program.cs for both APIs with Swagger setup.
    - Developed controllers for managing movies and to-do items.
    - Defined data models for movies and to-do items.
    - Updated launchSettings.json for local development.

---
+++ MovieReviewApi/Controllers/MovieReviewController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieReviewApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieReviewController : ControllerBase
+    {
+        private static List<Movie> _movies = new List<Movie>();
+
+        [HttpPost]
+        public IActionResult newMovie([FromBody] string name)
+        {
+            if (name == null) return BadRequest("Movie name cannot be empty");
+
+            var maxId = _movies.Count > 0 ? _movies.Max(m => m.id) : 0;
+
+            Movie m = new Movie(maxId + 1,name);
+
+            _movies.Add(m);
+
+            return Ok("Movie added");
+        }
+
+        [HttpPost("review/{id}")]
+        public IActionResult movieReview(int id, [FromBody] List<string> review)
+        {
+            var movie = _movies.FirstOrDefault(m => m.id == id);
+            if (movie == null) return BadRequest("Movie not found");
+
+            movie.Reviews = review;
+
+            return Ok("Review added");
+        }
+
+        [HttpGet]
+        public IActionResult getMovies()
+        {
+            return Ok(_movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getMovieById(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.id == id);
+
+            if (movie == null) return BadRequest("Movie not found");
+
+            return Ok(movie);
+        }
+    }
+
+    public class Movie
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<string> Reviews { get; set; }
+
+        public Movie(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private static List<ToDo> _toDos = new List<ToDo>();
+
+        [HttpPost]
+        public IActionResult NewTodo([FromBody] string title)
+        {
+            if (title == null) return BadRequest();
+            if (string.IsNullOrEmpty(title)) return BadRequest("Title is necessary");
+
+            var maxId = _toDos.Count > 0 ? _toDos.Max(t => t.id): 0;
+
+            var todo = new ToDo(maxId+1, title);
+
+            _toDos.Add(todo);
+            return Ok("Todo Added");
+        }
+
+        [HttpGet]
+        public IActionResult AllTodo()
+        {
+            return Ok(_toDos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTodoById(int id)
+        {
+            var todo = _toDos.FirstOrDefault(t => t.id == id);
+
+            if(todo == null) return NotFound("Todo not found");
+            
+            return Ok(todo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTodo(int id, [FromBody] ToDo UpdatedTodo)
+        {
+            var todo = _toDos.FirstOrDefault(t => t.id == id);
+
+            if(todo == null) return NotFound("Todo not found");
+
+            todo.Title = UpdatedTodo.Title;
+            todo.IsDone = UpdatedTodo.IsDone;
+            
+            return Ok("Todo updated successfully");
+        }
+
+        [HttpPatch("changeTitle/{id}")]
+        public IActionResult PartialTodoUpdate(int id, [FromBody] string title)
+        {
+            var todo = _toDos.FirstOrDefault(t => t.id == id);
+
+            if (todo == null) return NotFound("Todo not found");
+            if (string.IsNullOrEmpty(title)) return BadRequest("Please provide the todo title");
+
+            todo.Title = title;
+            
+            return Ok("Todo is updated successfully");
+        }
+
+        [HttpPatch("markComplete/{id}")]
+        public IActionResult MarkComplete(int id)
+        {
+            var todo = _toDos.FirstOrDefault(t => t.id == id);
+
+            if (todo == null) return NotFound("Todo not found");
+
+            todo.IsDone = true;
+
+            return Ok($"{id} marked completed");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTodo(int id)
+        {
+            var todo = _toDos.FirstOrDefault(t => t.id == id);
+
+            if (todo == null) return NotFound("Todo not found");
+
+            _toDos.Remove(todo);
+
+            return Ok("Todo deleted successfully");
+        }
+    }
+
+    public class ToDo
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+
+        public ToDo(int id, string title)
+        {
+            this.id = id;
+            Title = title;
+            IsDone = false;
+        }
+    }
+}
